035ce2ef9325de7406021ed04f1477aea84d0285
Mijail Rondon <mijail@riseup.net>
2018-10-19 02:13:46 +0200

    Deploy staging

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,11 @@ set :application, "dcm"
 set :repo_url, "https://github.com/mijailr/dcm"
 set :deploy_to, "/home/ruby-data/app"
 
-set :passenger_restart_with_touch, true
 set :rbenv_type, :user
 
 append :linked_files, "config/application.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+set :puma_bind, "tcp://0.0.0.0:3000"
+set :puma_user, fetch(:user)
+
